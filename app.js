dd5845a00460d1ee6cebc3da2bc9e5e9e7adc06b
DevOp0101 <robert.wooden101@gmail.com>
2024-02-23 18:28:51 -0600

    Updated Cors

--- app.js
+++ app.js
@@ -15,7 +15,9 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-app.use(cors());
+app.use(cors({
+  origin: 'https://devop0101.github.io/Frontend'
+}));
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
